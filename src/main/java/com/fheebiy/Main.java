a2f2b44242c109cb8481a8914e9e58cef3ed6f60
zhouwenbo <zhouwenbo550@163.com>
2014-06-02 16:21:05 +0800

    1234

---
+++ src/main/java/com/fheebiy/Main.java
@@ -0,0 +1,11 @@
+package com.fheebiy;
+
+/**
+ * Created by Administrator on 14-6-2.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.print(12345);
+    }
+}
